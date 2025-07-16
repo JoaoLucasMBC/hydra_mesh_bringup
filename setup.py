081d2c53551ed3060cdecdd1379ea51aab58fc5f
Joao Lucas <joaolucasmbcadorniga@gmail.com>
2025-07-16 10:39:30 -0500

    feat: first version done working

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'hydra_mesh_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), ['launch/my_custom_simulation_launch.py']),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf.xacro')),
+        # --- REMOVE THIS LINE: (os.path.join('share', package_name, 'worlds'), glob('worlds/*.stl')),
+        # --- ADD THESE NEW LINES FOR office_model ---
+        (os.path.join('share', package_name, 'worlds', 'office_model'), glob('worlds/office_model/*.*')), # model.config, model.sdf
+        (os.path.join('share', package_name, 'worlds', 'office_model',
+                      'meshes'), glob('worlds/office_model/meshes/*.dae')), # Change the extension
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='João Lucas',
+    maintainer_email='joaolmbc@al.insper.edu.br',
+    description='Bringup package using the Hydra-generated mesh in Gazebo',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
